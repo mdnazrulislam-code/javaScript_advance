c437f604b35336b308b0fe06fcc41dd50353928c
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-02 23:06:33 +0000

    variable added

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "arfanislamabir@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+/*
+Prefer not to use var
+
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
-console.log("Hitesh")
+const accountId = 144553
+let accountEmail = "arfanislamabir@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountId = 2
+
+console.log(accountId);
