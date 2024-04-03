923ad713e00c4bab7621aa56b8e9cbc39726d8db
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-03 19:39:43 +0000

    reduce

---
+++ 05_iterations/nine.js
@@ -0,0 +1,33 @@
+const myNums = [1,2,3,4,5,6,7]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// const myTotal = myNums.reduce((acc, curr)=>acc + curr,0)
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "JS Course",
+        price: 2999
+    },
+    {
+        itemName: "Python Course",
+        price: 999
+    },
+    {
+        itemName: "Mobile Dev Course",
+        price: 5999
+    },
+    {
+        itemName: "Data Science Course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item)=>(acc +item.price),0)
+
+console.log(priceToPay);
