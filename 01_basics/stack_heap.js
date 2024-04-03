f6b8f84283b62c82fee5c9dba8bddc543cadc566
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-03 08:46:32 +0000

    stack and heap memory

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,11 @@
+// Stack Memory(Primitive),   Heap(Non-Primitive)
+
+
+let myYouTubeName = "Nazruldotcom"
+let anothername = myYouTubeName
+
+anothername = " chaiaurcode"
+
+console.log(myYouTubeName);
+console.log(anothername);
+
