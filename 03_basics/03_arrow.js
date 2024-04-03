5f04c536d2a3d8ee00ea90df08f1e7bf4e696695
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-03 15:22:35 +0000

    Arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,10 +4,38 @@ const user = {
 
     welcomeMessage: function(){
         console.log(`${this.username} welcome to website`);
+        console.log(this);
     }
+   
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+// function chai(){
+//     let username = "Hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai =()=>{
+    let username ="Hitesh"
     console.log(this);
 }
 
-user.welcomeMessage()
-user.username = "Sam"
-user.welcomeMessage()
+// chai()
+
+// const addTwo = (num1, num2)=>{
+//     return num1 + num2
+// }
+// const addTwo = (num1, num2)=> num1 + num2
+// const addTwo = (num1, num2)=> (num1 + num2)
+const addTwo = (num1, num2)=> ({username: "hitesh"})
+
+
+console.log(addTwo(3,4));
