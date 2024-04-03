c94dd3d402446d5da48831cabbd0753a189c2488
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-03 13:40:24 +0000

    hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,17 +1,54 @@
-let a = 300
+// let a = 300
 
-if(true){
-    let a = 10
-    const b = 20
-    console.log("INNER", a);
-}
+// if(true){
+//     let a = 10
+//     const b = 20
+//     console.log("INNER", a);
+// }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
+// }
+
+
+// // console.log(a);
+// // console.log(b);
+// console.log(a);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
 }
 
+// one()
 
-// console.log(a);
-// console.log(b);
-console.log(a);
+if (true){
+    const username = "hitesh"
+    if (username == "hitesh"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(addOne(5))
+function addOne(num){
+    return num +1
+}
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
