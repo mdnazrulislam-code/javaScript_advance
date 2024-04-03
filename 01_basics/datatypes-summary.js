e34d12856b247f2d57c83961ca13bbf268c6fa75
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-03 00:04:08 +0000

    more about data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,49 @@
+// Primitive data type
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id);
+// console.log(anotherId);
+// console.log(id === anotherId);
+
+// const  bigNumber  = 3555255545263665n
+
+
+
+
+
+
+
+
+
+// Reference data type or non-primitive data type
+
+// Array, Objects, Functions, 
+
+const  heros = ["shaktiman", "naagraj", "doga"]
+
+let myobj = {
+    name: "abir",
+    age: 22
+}
+
+console.log(myobj);
+
+
+const myFunction = function(){
+    console.log("Hello World!");
+}
+
+
+myFunction()
