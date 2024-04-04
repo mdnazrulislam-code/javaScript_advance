0fd67c7816ec24a05c5adf51478d94e3bcbb58b9
Md Nazrul Islam <75695509+mdnazrulislam-code@users.noreply.github.com>
2024-04-04 17:55:40 +0000

    calls

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,16 @@
+function SetUserName(username){
+    // Complex DB calls 
+    this.username = username
+    console.log("Called");
+}
+
+function createUser(username, email, password){
+
+    SetUserName.call(this,username)
+    
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
